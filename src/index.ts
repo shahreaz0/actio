c4619f88ce3d987d5c813af92195c805f3eca2a5
shahreaz0 <ashahreaz@gmail.com>
2024-12-02 00:24:10 +0600

    chore: setup biome as linter and formatter

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server"
+import { Hono } from "hono"
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get("/", (c) => {
+  return c.text("Hello Hono!")
 })
 
 const port = 3000
@@ -12,5 +12,5 @@ console.log(`Server is running on http://localhost:${port}`)
 
 serve({
   fetch: app.fetch,
-  port
+  port,
 })
